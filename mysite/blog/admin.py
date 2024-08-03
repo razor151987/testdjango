62be5edc6e9514354fffb73ea9bfce8f1f187801
MichelRomijn <mailnaar@michelromijn.nl>
2024-08-03 13:25:59 +0200

    Fout in code

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
-from .models import Voetballers
+from .models import Voetbalspelers
 
 
 admin.site.register(Post)
-admin.site.register(Voetballers)
+admin.site.register(Voetbalspelers)
