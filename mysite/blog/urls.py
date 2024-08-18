a06f03a0338cf8d11c77b71ea0b1f3935752a770
MichelRomijn <mailnaar@michelromijn.nl>
2024-08-18 07:57:13 +0200

    Les 6 Django NHA

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf.urls import include
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_inhoud'),
+    path('post/nieuw/', views.post_nieuw, name='post_nieuw'),
+    path('delete/<post_id>', views.delete_post, name='delete'),
+
+
 ]
